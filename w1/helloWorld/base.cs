385cba65ac1b72e7deba08eaaacc6fb30e638a18
Salvanim <dylanfish05@icloud.com>
2024-03-28 09:13:10 -0400

    helloWorld

---
+++ w1/helloWorld/base.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const string name = "Dylan";
+      System.Console.WriteLine("Hello " + name + "!");
+    }
+  }
+}
