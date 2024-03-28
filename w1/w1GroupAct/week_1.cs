6dadddb1aaf9f144f126ea34a0ec8800f962522d
Salvanim <dylanfish05@icloud.com>
2024-03-28 11:01:34 -0400

    groupPreLab

--- w1/w1GroupAct/week_1.cs
+++ w1/w1GroupAct/week_1.cs
@@ -25,42 +25,42 @@ namespace w1GroupAct {
             strNum1 = Console.ReadLine();
 
             Console.Write ("Please enter the math operation (PLUS, MINUS, MULTIPLY, DIVIDE): ");
-            strOperand = Console.ReadLine().ToLower();
+            strOperand = Console.ReadLine().ToUpper();
 
             Console.Write ("Please enter the second number: ");
             strNum2 = Console.ReadLine();
 
             //Parses provided numbers to ints
-            intNum1 = Int32.Parse(strNum1);
+            intNum1 = Convert.ToInt32 (strNum1);
             intNum2 = Convert.ToInt32 (strNum2);
 
             //Performs opperations based on users input
             switch (strOperand) {
-                case "plus" or "+":
+                case "PLUS" or "+":
                     intResult = intNum1 + intNum2;
                     break;
-                case "minus" or "-":
+                case "MINUS" or "-":
                     intResult = intNum1 - intNum2;
                     break;
-                case "divide" or "/":
+                case "DIVIDE" or "/":
                     intResult = intNum1 / intNum2;
                     break;
-                case "multply" or "*":
+                case "MULTIPLY" or "*":
                     intResult = intNum1 * intNum2;
                     break;
             }
 
-            //Parses oppertion result
+            //Casts result as Double
             dblResult = (Double) intResult;
 
             //Returns result to console
-            if (strOperand == "plus" || strOperand == "+"){
+            if (strOperand == "PLUS" || strOperand == "+"){
                 Console.WriteLine ($"\n\nThe sum of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "minus" || strOperand == "-"){
+            } else if (strOperand == "MINUS" || strOperand == "-"){
                 Console.WriteLine ($"\n\nThe difference of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "divide" || strOperand == "/"){
+            } else if (strOperand == "DIVIDE" || strOperand == "/"){
                 Console.WriteLine ($"\n\nThe quotient of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "multiply" || strOperand == "*")
+            } else if (strOperand == "MULTIPLY" || strOperand == "*")
                 Console.WriteLine ($"\n\nThe product of {intNum1} and {intNum2} equals: {dblResult}");
 
             //Exits program
