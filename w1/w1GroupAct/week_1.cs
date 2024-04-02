bd1f5e58d3afc4edf0e5429de6e98a70c6a486ab
Salvanim <dylanfish05@icloud.com>
2024-04-02 09:28:56 -0400

    betterCalc

--- w1/w1GroupAct/week_1.cs
+++ w1/w1GroupAct/week_1.cs
@@ -9,8 +9,27 @@ namespace w1GroupAct {
         static void Main (string[] args) {
             //Initialzes Varriables
             String strFirst, strOperand, strNum2, strNum1;
-            Double intNum1 = 0, intNum2 = 0, intResult = 0;
-            Double dblResult;
+            Dictionary<string, string> opperands = new Dictionary<string, string>(){
+                {"PLUS","+"},
+                {"MINUS","-"},
+                {"MULTIPLY","*"},
+                {"DIVIDE","/"},
+                {"MODULUS","%"},
+                {"EXPONENT","^"},
+                {"POWER","^"}
+            };
+
+            Dictionary<string, string> resultantName = new Dictionary<string, string>(){
+                {"+","sum"},
+                {"-","difference"},
+                {"*","product"},
+                {"/","quotient"},
+                {"%","remainder"},
+                {"^","power"},
+            };
+            
+            Double intNum1 = 0, intNum2 = 0;
+            Double dblResult = 0;
 
             //Gets user's name and addresses user
             Console.WriteLine ("Hello There!");
@@ -23,8 +42,7 @@ namespace w1GroupAct {
             //Gets numbers and opperand for opperation from user
             Console.Write ("Please enter the first number: ");
             strNum1 = Console.ReadLine();
-
-            Console.Write ("Please enter the math operation (PLUS, MINUS, MULTIPLY, DIVIDE): ");
+            Console.Write ($"Please enter the math operation ({string.Join(", ", new List<string>(opperands.Keys))}): ");
             strOperand = Console.ReadLine().ToUpper();
 
             Console.Write ("Please enter the second number: ");
@@ -35,34 +53,36 @@ namespace w1GroupAct {
             intNum2 = Convert.ToDouble(strNum2);
 
             //Performs opperations based on users input
+            string result;
+            if(opperands.TryGetValue(strOperand, out result)){
+                strOperand = result;
+            }
+
             switch (strOperand) {
-                case "PLUS" or "+":
-                    intResult = intNum1 + intNum2;
+                case "+":
+                    dblResult = intNum1 + intNum2;
                     break;
-                case "MINUS" or "-":
-                    intResult = intNum1 - intNum2;
+                case "-":
+                    dblResult = intNum1 - intNum2;
                     break;
-                case "DIVIDE" or "/":
-                    intResult = intNum1 / intNum2;
+                case "/":
+                    dblResult = intNum1 / intNum2;
                     break;
-                case "MULTIPLY" or "*":
-                    intResult = intNum1 * intNum2;
+                case "*":
+                    dblResult = intNum1 * intNum2;
+                    break;
+                case "%":
+                    dblResult = intNum1 % intNum2;
+                    break;
+                case "^":
+                    dblResult = Math.Pow(intNum1,intNum2);
                     break;
             }
 
-            //Casts result as Double
-            dblResult = (Double) intResult;
-
             //Returns result to console
-            if (strOperand == "PLUS" || strOperand == "+"){
-                Console.WriteLine ($"\n\nThe sum of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "MINUS" || strOperand == "-"){
-                Console.WriteLine ($"\n\nThe difference of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "DIVIDE" || strOperand == "/"){
-                Console.WriteLine ($"\n\nThe quotient of {intNum1} and {intNum2} equals: {dblResult}");
-            } else if (strOperand == "MULTIPLY" || strOperand == "*")
-                Console.WriteLine ($"\n\nThe product of {intNum1} and {intNum2} equals: {dblResult}");
-
+            if(resultantName.TryGetValue(strOperand, out result)){
+                Console.WriteLine ($"\n\nThe {result} of {intNum1} and {intNum2} equals: {dblResult}");
+            }
             //Exits program
             Console.WriteLine ("\n\nPress Any Key to Continue");
             Console.ReadKey ();
