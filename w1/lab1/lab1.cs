3ca30b5aa42be73b68a127e9f444597931c376cd
Salvanim <dylanfish05@icloud.com>
2024-04-04 09:01:48 -0400

    w2Sample1

---
+++ w1/lab1/lab1.cs
@@ -0,0 +1,32 @@
+﻿using System
+
+namespace Lab1 {
+    class Program{
+        static void Main(string[] args){
+            string studentName, anotherGrade;
+            List<int> grades = new List<int>(){};
+            
+            Console.Write("Please enter the students name: ");
+            studentName = Console.ReadLine();
+
+            anotherGrade = "Y"
+            while(anotherGrade == "Y"){
+               Console.Write("Please enter a grade of the student (as %): ");
+               int grade;
+               bool isINT = int.TryParse(Console.ReadLine(), out grade);
+               if(!isInt){
+                Console.Write("Not a Number! Please enter a grade of the student (as %): ");
+                bool isINT = int.TryParse(Console.ReadLine(), out grade);
+               }
+               List.add(grade);
+               Console.Write("Do you want to add another grade (Y/N): ");
+               anotherGrade = Console.ReadLine().ToUpper();
+            }
+
+            for(int i; i < grades.Count(); i++){
+                Console.WriteLine(grades[i]);
+            }
+
+        }
+    }
+}
