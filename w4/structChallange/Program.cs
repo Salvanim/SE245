41f5a27738b71eb0dd4a214b41801bdc5f0467a5
Salvanim <dylanfish05@icloud.com>
2024-04-16 09:26:05 -0400

    challange

---
+++ w4/structChallange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace structChallange
+{
+  class Program{
+    struct Point{
+        public int X;
+        public int Y;
+    }
+
+    static Point GetRandomPoint(int minX, int minY, int maxX, int maxY){
+        Random rnd = new Random();
+        Point ranPoint = new Point();
+        ranPoint.X = rnd.Next(minX, maxX);
+        ranPoint.Y = rnd.Next(minY, maxY);
+        return ranPoint;
+    }
+
+    static void Main(string[] args)
+    {
+        int minimumX = 10;
+        int minimumY = 10;
+        string cont = "";
+        while(cont == ""){
+            Point RandomPoint = GetRandomPoint(-minimumY, -minimumY, minimumX, minimumY);
+            Console.Write($"({RandomPoint.X},{RandomPoint.Y})");
+            cont = Console.ReadLine();
+        }
+    }
+  }
+}
