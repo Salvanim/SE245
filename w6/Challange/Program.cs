66d18ce55473534445be504ae1e87552b673e041
Salvanim <dylanfish05@icloud.com>
2024-04-30 09:35:37 -0400

    w6

---
+++ w6/Challange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq; 
+
+namespace midterm
+{
+  class Program{
+
+    static void Main(string[] args){
+        Console.WriteLine(isPalindrome("Race Car").ToString());
+    }
+
+    public bool isPalindrome(string testString){
+        testString = testString.ToUpper().Replace(" ", "");
+        string reversedString = "";
+        for(int i = testString.Length; i > 0; i--){
+            reversedString = testString[i].ToString();
+        }
+        if(reversedString == testString){
+            return true;
+        }
+        return false;
+    }
+
+    public string onlyBaseText(string stringToConvert){
+        return stringToConvert.ToUpper().Replace(" ", "");
+    }
+
+  }
+}
